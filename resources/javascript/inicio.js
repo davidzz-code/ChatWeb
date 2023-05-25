38406c6efbd61669abaf151965650284bc3f998a
David Ramírez <rrdavid08@gmail.com>
2023-05-25 20:35:40 +0200

    Añadida función doPost()

--- resources/javascript/inicio.js
+++ resources/javascript/inicio.js
@@ -9,3 +9,16 @@ function comprobarContraseña() {
     }
 }
     // FALTA AÑADIR QUE SI LAS CONTRASEÑAS NO COINCIDEN NO SE PUEDA ENVIAR
+
+function enviarRegistro() {
+    var http = new XMLHttpRequest();
+
+    let user = document.getElementById("user");
+    let mail = document.getElementById("mail");
+    let pass = document.getElementById("pass");
+    let codeCountry = document.getElementById("codeCountry");
+
+    http.open("POST", "http://localhost:5000/XatLLM/Register");
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.send("user=" + user + "&pass=" + pass + "&mail=" + mail + "&codeCountry=" + codeCountry);
+}
