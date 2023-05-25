8d3690ca4713d6227bfc33b9c3854445177d3b08
David Ramírez <rrdavid08@gmail.com>
2023-05-25 20:24:42 +0200

    Función comprobar contraseña añadida

--- resources/javascript/inicio.js
+++ resources/javascript/inicio.js
@@ -1,14 +1,11 @@
 function comprobarContraseña() {
     let pass = document.getElementById("pass").value;
-    let checkPass = document.getElementById("checkPassInput").value;
+    let checkPass = document.getElementById("checkPass").value;
 
     if (pass != checkPass) {
-        document.getElementById("checkPassLabel").style.color = "red";
-        document.getElementById("checkPassLabel").style.fontStyle = "italic";
-        document.getElementById("checkPassInput").style.border = "1px solid red";
+        document.getElementById("checkPass").style.border = "1px solid red";
     } else {
-        document.getElementById("checkPassLabel").style.color = "black";
-        document.getElementById("checkPassLabel").style.fontStyle = "normal";
-        document.getElementById("checkPassInput").style.border = "1px solid black";
+        document.getElementById("checkPass").style.border = "1px solid black";
     }
 }
+    // FALTA AÑADIR QUE SI LAS CONTRASEÑAS NO COINCIDEN NO SE PUEDA ENVIAR
