1964abdfbb0e97b78a3bdb43938a35d55b21f355
David Ramírez <rrdavid08@gmail.com>
2023-05-27 19:47:33 +0200

    Función Añadir amigos a la lista funciona

--- resources/javascript/chat.js
+++ resources/javascript/chat.js
@@ -10,16 +10,15 @@ function añadirAmigo() {
     http.send("mail="+mail+"&session="+session+"&friend="+friend);
     
     http.onreadystatechange = function () {
-
         if (http.readyState == 4 && http.status == 200) {
-            
             let respuesta = http.responseText;
             
             if (respuesta == 0) {
                 console.log("ERROR. El servidor no responde");
             } else if (respuesta == 1) {                
                 console.log("Todo bien")
-                
+                recibirAmigos();
+
             } else if (respuesta == 2) {
                 console.log("Amigo no encontrado");
             } else if (respuesta == 3) {
@@ -35,5 +34,27 @@ function añadirAmigo() {
 function recibirAmigos() {
     let http = new XMLHttpRequest();
 
-    let
+    let mail = sessionStorage.getItem("mail");
+    let session = sessionStorage.getItem("session");
+
+    http.open("GET", "http://localhost:5000/XatLLM/Friend?mail="+mail+"&session="+session);
+    http.send();
+
+    http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+            let jsonAmigos = JSON.parse(http.responseText);
+
+            let select = document.getElementById("listaAmigos");
+            let option = document.createElement("option");
+
+            for (let i = 0; i < jsonAmigos.length; i++){
+                
+                option.text = jsonAmigos[i];
+                option.value = jsonAmigos[i];
+                select.add(option);
+            }
+        }
+    }
+
+
 }
