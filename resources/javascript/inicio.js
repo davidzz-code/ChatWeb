55a646cfd523225d1ae39606c969d06a523717e4
David Ramírez <rrdavid08@gmail.com>
2023-05-27 17:12:38 +0200

    Añadidos paises JSON, funciona

--- resources/javascript/inicio.js
+++ resources/javascript/inicio.js
@@ -13,12 +13,46 @@ function comprobarContraseña() {
 function enviarRegistro() {
     var http = new XMLHttpRequest();
 
-    let user = document.getElementById("user");
-    let mail = document.getElementById("mail");
-    let pass = document.getElementById("pass");
-    let codeCountry = document.getElementById("codeCountry");
+    let user = document.getElementById("user").value;
+    let mail = document.getElementById("mail").value;
+    let pass = document.getElementById("pass").value;
+    let codeCountry = document.getElementById("codeCountry").value;
 
-    http.open("POST", "http://localhost:5000/XatLLM/Register");
+    http.open("POST", "http://localhost:5000/ChatWeb_Backend/Register", true);
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    http.send("user=" + user + "&pass=" + pass + "&mail=" + mail + "&codeCountry=" + codeCountry);
+    http.send("user="+user+"&mail="+mail+"&pass="+pass+"&codeCountry="+codeCountry);
+
+    // Respuesta
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) { // Si el atributo Status es igual a 200 significa que HA IDO TODO BIEN
+            console.log("Registro completado: " + http.responseText);
+            document.getElementById("result").innerHTML = "Te has registrado";
+        }
+    }
+}
+
+function getCountries() {
+    let http = new XMLHttpRequest();
+
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("ready");
+
+            let jsonString = http.responseText;
+
+
+            let countryList = JSON.parse(jsonString);
+            console.log(countryList);
+
+            for (let i = 0; i < countryList.length; i++){
+                let select = document.getElementById("codeCountry");
+                let option = document.createElement("option");
+                option.text = countryList[i].name;
+                option.value = countryList[i].code;
+                select.add(option);
+            }
+        }
+    }
+    http.open("GET", "http://localhost:5000/ChatWeb_Backend/Register", true);
+    http.send();
 }
