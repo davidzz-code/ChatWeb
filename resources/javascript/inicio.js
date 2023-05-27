e8d07160c6e7568f7484df3b03d8e4438d717880
David Ramírez <rrdavid08@gmail.com>
2023-05-27 18:06:03 +0200

    Añadido método Iniciar Sesión

--- resources/javascript/inicio.js
+++ resources/javascript/inicio.js
@@ -18,7 +18,7 @@ function enviarRegistro() {
     let pass = document.getElementById("pass").value;
     let codeCountry = document.getElementById("codeCountry").value;
 
-    http.open("POST", "http://localhost:5000/ChatWeb_Backend/Register", true);
+    http.open("POST", "http://localhost:5000/XatLLM/Register", true);
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     http.send("user="+user+"&mail="+mail+"&pass="+pass+"&codeCountry="+codeCountry);
 
@@ -27,6 +27,29 @@ function enviarRegistro() {
         if (this.readyState == 4 && this.status == 200) { // Si el atributo Status es igual a 200 significa que HA IDO TODO BIEN
             console.log("Registro completado: " + http.responseText);
             document.getElementById("result").innerHTML = "Te has registrado";
+        }
+    }
+}
+
+function enviarLogin() {
+    let http = new XMLHttpRequest();
+    let mail = document.getElementById("mail").value;
+    let pass = document.getElementById("pass").value;
+
+    http.open("GET", "http://localhost:5000/XatLLM/Login?mail="+mail+"&pass="+pass);
+    http.send();
+
+
+
+    http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+            let session = this.responseText;
+            if (session > 0) {
+                window.sessionStorage.setItem("mail", mail);
+                window.sessionStorage.setItem("pass", pass);
+            } else {
+                document.querySelector(".result").innerHTML = "Login INCORRECTO";
+            }
         }
     }
 }
@@ -53,6 +76,6 @@ function getCountries() {
             }
         }
     }
-    http.open("GET", "http://localhost:5000/ChatWeb_Backend/Register", true);
+    http.open("GET", "http://localhost:5000/XatLLM/Register", true);
     http.send();
 }
