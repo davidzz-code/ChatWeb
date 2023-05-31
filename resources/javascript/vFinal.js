d5f1b8f018f9b9397dda37e383bfa4f5180ccf74
David Ramírez <rrdavid08@gmail.com>
2023-05-31 02:40:41 +0200

    "añadirDiv" y "elegirChat" agregado

--- resources/javascript/vFinal.js
+++ resources/javascript/vFinal.js
@@ -1,8 +1,32 @@
 function añadirDiv() {
     let divAmigos = document.querySelector(".contenedor-amigos");
-
     let emailAmigo = document.getElementById("friend").value;
-    let insertarDiv = "<div class=\"div-amigo\">" + emailAmigo + "</div>"
+
+
+    let nuevoDiv = document.createElement("div");
+    nuevoDiv.textContent = emailAmigo;
+    nuevoDiv.classList.add("div-amigo");
+    nuevoDiv.addEventListener("click", elegirChat);
     
-    divAmigos.innerHTML += insertarDiv;
+    divAmigos.appendChild(nuevoDiv);
+    document.getElementById("friend").value = "";
+    
+    nuevoDiv.style.backgroundColor = "lightgrey";
+}
+
+function elegirChat() {
+    let colorChat = window.getComputedStyle(document.querySelector(".contenedor-principal")).backgroundColor;
+    let todosLosAmigos = document.querySelectorAll(".div-amigo");
+
+
+    for (let divAmigo of todosLosAmigos) {
+        divAmigo.classList.remove("seleccionado");
+        divAmigo.style.backgroundColor = "lightgray";
+    }
+
+    let chatElegido = this;
+    chatElegido.classList.add("seleccionado");
+    chatElegido.style.backgroundColor = colorChat;
+    chatElegido.style.borderRight = "none";
+
 }
